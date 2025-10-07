5bb2390f8c280cef97620f31c495c298ea6fd56c
Sahil Gupta <guptas3067@gmail.com>
2025-10-07 13:02:42 +0530

    a

---
+++ src/routes/api/index.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute } from "@tanstack/react-router"
+
+export const Route = createFileRoute('/api/')({
+  server: {
+    handlers: {
+      POST: async ({ request }) => {
+        const body = await request.json()
+        return new Response(JSON.stringify({ message: `Hello, ${body.name}!` }))
+      },
+    },
+  },
+})
